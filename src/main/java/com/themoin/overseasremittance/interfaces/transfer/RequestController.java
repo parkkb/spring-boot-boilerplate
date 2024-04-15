e7a399a9f3fd1137ad44e040d454ccf981340405
박광배 <gbpark6387@jobkorea.co.kr>
2024-04-15 17:11:41 +0900

    송금 견적서 저장.

--- src/main/java/com/themoin/overseasremittance/interfaces/transfer/RequestController.java
+++ src/main/java/com/themoin/overseasremittance/interfaces/transfer/RequestController.java
@@ -1,10 +1,20 @@
 package com.themoin.overseasremittance.interfaces.transfer;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.themoin.overseasremittance.common.response.ResultVo;
+import com.themoin.overseasremittance.interfaces.transfer.request.RequestDto;
 
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
 public class RequestController {
+
+	@PostMapping("/transfer/request")
+	public ResultVo transferRequest(RequestDto requestDto){
+
+		return null;
+	}
 }
