b1a651856ea689219ed8c06775b49e7aacdef0b5
박광배 <gbpark6387@jobkorea.co.kr>
2024-05-28 17:53:27 +0900

    MockBean을 이용해서 service test 구현

--- src/test/java/com/parkkb/domain/user/LogInServiceTest.java
+++ src/test/java/com/parkkb/domain/user/LogInServiceTest.java
@@ -1,44 +1,31 @@
 package com.parkkb.domain.user;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
-import com.parkkb.infrastructure.user.UserService;
-import com.parkkb.interfaces.user.enums.IdType;
 import com.parkkb.interfaces.user.request.LoginDto;
-import com.parkkb.interfaces.user.request.UserRegistrationDto;
-import com.parkkb.interfaces.user.response.LoginResultVo;
 
 @SpringBootTest
 class LogInServiceTest {
 
-	@Autowired
+	@MockBean
 	private LogInService logInService;
-
-	@Autowired
-	private UserService userService;
-
-	@Autowired
-	private SignUpService signUpService;
 
 	@DisplayName("정상적인 로그인 테스트")
 	@Test
 	void loginTest() {
 
-		//given
-		UserRegistrationDto userRegistrationDto = new UserRegistrationDto("abcd", "1234", "abc", IdType.REG_NO,
-				"1231");
-		signUpService.signUp(userRegistrationDto);
 
+		//given
 		LoginDto loginDto = new LoginDto("abcd", "1234");
 		//when
-		LoginResultVo loginResultVo = logInService.login(loginDto);
+		logInService.login(loginDto);
 
 		//then
-		Assertions.assertEquals(loginResultVo.getResultCode(), 200);
+		Mockito.verify(logInService).login(loginDto);
 	}
 
 }
