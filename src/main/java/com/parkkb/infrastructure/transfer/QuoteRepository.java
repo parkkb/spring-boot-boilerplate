d1b3e59cb72b3d79326cc4d5396e354e49fca0c9
박광배 <gbpark6387@jobkorea.co.kr>
2024-06-03 17:11:17 +0900

    multi module로 프로젝트 구조 변경 - module-api 생성

--- src/main/java/com/parkkb/infrastructure/transfer/QuoteRepository.java
+++
@@ -1,6 +0,0 @@
-package com.parkkb.infrastructure.transfer;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuoteRepository extends JpaRepository<Quote, Long> {
-}
